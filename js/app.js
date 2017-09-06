10bd367dc36c5b3ab67571d3ceab9d96c10ce7a3
nstalev <stalev_a@abv.bg>
2017-09-06 11:30:53 +0300

    add Register, Login, Logout

---
+++ js/app.js
@@ -0,0 +1,182 @@
+$(()=>{
+
+     // ---ATTACH EVENT HANDLERS
+     (()=>{
+
+       $('nav').find('a[data-target]').click(navigateTo);
+       //  console.log($('.container').find('button[data-target]'));
+         $('.container').find('button[data-target]').click(navigateTo);
+
+
+         $('#formRegister').submit(registerUser);
+         $('#formLogin').submit(loginUser);
+         $('#linkMenuLogout').click(logOutUser);
+
+
+    })();
+
+
+    function navigateTo() {
+        let dataTarget = $(this).attr('data-target');
+
+        showView(dataTarget);
+    }
+
+
+    //--- REGISTER USER---
+    function registerUser(ev) {
+
+        ev.preventDefault();
+
+        let registerUsername= $('#username-register');
+        let registerEmail = $('#email-register');
+        let registerPassword = $('#pwd-register');
+        let registerConfirmPassword = $('#pwd-confirm-register');
+
+        let username = registerUsername.val();
+        let email = registerEmail.val();
+        let password = registerPassword.val();
+        let confirmPassword = registerConfirmPassword.val();
+
+        if(validation(username,email,password,confirmPassword)){
+            auth.register(username, password, email)
+                .then((userInfo) =>{
+                    registerEmail.val('');
+                    registerUsername.val('');
+                    registerPassword.val('');
+                    registerConfirmPassword.val('');
+                    saveSession(userInfo);
+                    showInfo("Register successful." );
+                    showView('game-menu');
+                })
+        }
+    }
+
+
+    //--- LOGIN USER ---
+    function loginUser(ev) {
+        ev.preventDefault();
+        let loginUsername= $('#username-login');
+        let loginPasswd = $('#pwd-login');
+
+        let username = loginUsername.val();
+        let password =  loginPasswd.val();
+
+        auth.login(username, password)
+            .then((userInfo) =>{
+                loginUsername.val('');
+                loginPasswd.val('');
+                saveSession(userInfo);
+                showInfo("User login successful." );
+                showView('game-menu');
+            })
+
+    }
+
+    //--- LOGOUT USER ---
+    function logOutUser() {
+        auth.logout()
+            .then(() =>{
+                sessionStorage.clear();
+                showInfo('LogOut successful');
+                userLoggedOut();
+            })
+            .catch(handleError)
+    }
+
+    //--- VALIDATIONS ---
+    function validation(username, email, password, confirmPassword) {
+        if(username.length < 3 ){
+            showError('username should be at least 3 characters');
+            return false
+        }
+        if(password.length < 4 ){
+            showError('password should be at least 4 characters');
+            return false
+        }
+        if(password !== confirmPassword){
+            showError("password doesn't match");
+            return false
+        }
+        return true
+    }
+
+
+
+
+
+    if(sessionStorage.getItem('authtoken') === null){
+        userLoggedOut();
+    }else{
+        userLoggedIn();
+    }
+
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+       // console.log(viewName);
+        $('main > section').hide();
+        $('#section-' + viewName).show();
+    }
+    
+    
+    function userLoggedIn() {
+        $('.anonymous').hide();
+        $('.useronly').show();
+        let username = sessionStorage.getItem('username');
+        $('#spanMenuLoggedInUser').text(username);
+        $('#battleShipsLogo').click(() => {
+            showView('game-menu');
+        });
+        showView('game-menu');
+    }
+
+    function userLoggedOut() {
+        $('.anonymous').show();
+        $('.useronly').hide();
+        $('#spanMenuLoggedInUser').text('');
+        $('#battleShipsLogo').click(() => {
+            showView('appHome');
+        });
+        showView('appHome');
+    }
+
+
+
+    function saveSession(userInfo) {
+        let userAuth = userInfo._kmd.authtoken;
+        sessionStorage.setItem('authtoken', userAuth);
+        let userId = userInfo._id;
+        sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        sessionStorage.setItem('name', userInfo['name']);
+        userLoggedIn();
+    }
+
+    function handleError(reason) {
+        showError(reason.responseJSON.description);
+    }
+
+    function showInfo(message) {
+        let infoBox = $('#infoBox');
+        infoBox.text(message);
+        infoBox.show();
+        setTimeout(() => infoBox.fadeOut(), 3000);
+    }
+
+    function showError(message) {
+        let errorBox = $('#errorBox');
+        errorBox.text(message);
+        errorBox.show();
+        setTimeout(() => errorBox.fadeOut(), 3000);
+    }
+
+
+    // ---HANDLE NOTIFICATIONS----
+    $(document).on({
+        ajaxStart: () => $("#loadingBox").show(),
+        ajaxStop: () => $('#loadingBox').fadeOut()
+    });
+
+});
