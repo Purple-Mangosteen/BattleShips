619fc4e9770e4cc5cb29daedb8dfc3d79c3ab03e
Theodor <tiapko@abv.bg>
2017-09-10 00:56:42 +0300

    Game engine rebuild from scratch. Maps loading from the database. Randomization is not implemented.

--- js/game/battleship.js
+++ js/game/battleship.js
@@ -1,103 +1,111 @@
 let playground = (() => {
 
-let model = {
-    boardSize: 7,
-    numShips: 3,
-    shipLength: 3,
-    shipsSunk: 0,
+    const gameWinScore = 9;
+    const fields = 7 * 7;
+    const hitsToSink = 3;
 
-    ships:[
-        { locations: ["11", "12", "13"], hits: ["", "", ""] },
-        { locations: ["31", "32", "33"], hits: ["", "", ""] },
-        { locations: ["51", "52", "53"], hits: ["", "", ""] }
-    ],
+    let model = {
+        ships: [],
+        score: 0,
+        history: new Array(fields).fill('unknown')
 
-    fire: function(guess) {
-        for (let i = 0; i < this.numShips; i++) {
-            let ship = this.ships[i];
-            let index = ship.locations.indexOf(guess);
-            if (ship.hits[index] === "hit") {
-                view.displayMessage("You hit this ship before.");
-                return true;
-            } else if (index >= 0) {
-                ship.hits[index] = "hit";
-                view.displayHit(guess);
-                view.displayMessage("It's a hit!");
-                if (this.isSunk(ship)) {
-                    view.displayMessage("You sunk my battleship!");
-                    this.shipsSunk++;
+    };
+
+    let view = {
+        displayMessage: function (msg) {
+            let messageArea = document.getElementById("messageArea");
+            messageArea.innerHTML = msg;
+        },
+
+        displayHit: function (location) {
+            let cell = document.getElementById(location);
+            cell.setAttribute("class", "hit");
+
+        },
+
+        displayWinn: function () {
+            this.displayMessage('You won!');
+            $('table').hide();
+
+        },
+
+        displayMiss: function (location) {
+            let cell = document.getElementById(location);
+            cell.setAttribute("class", "miss");
+        }
+    };
+
+    function init() {
+
+        // hard coded map ID
+
+        // get ships location from the database
+        requester.get('appdata', 'gameBoards/59b456d4797e4f820497665f', '').then((mapInfo) => {
+            for (let i = 0; i < mapInfo.board.length; i++) {
+                let ship = {hits: 0, coordinates: mapInfo.board[i].coordinates};
+                model.ships.push(ship);
+            }
+        });
+
+        // hides game start button
+        $('#game-init-button').hide();
+
+        console.log('game initialization...');
+
+        let battleBlocks = $('td');
+        for (let block of battleBlocks) {
+            $(block).on('click', shoot);
+        }
+        view.displayMessage("Let's play! There are 3 ships, each 3 cells long");
+
+
+    }
+
+    function shoot() {
+        let fieldId = $(this).find('div:first').attr('id');
+        let result = 'miss';
+        // checks outcome of the shoot
+        for (let ship of model.ships) {
+            // checks if it hits
+            for (let i = 0; i < ship.coordinates.length; i++) {
+                if (ship.coordinates[i].hasOwnProperty(fieldId) && ship.coordinates[i][fieldId] === 'ship') {
+                    ship.coordinates[i][fieldId] = 'hit';
+                    ship.hits++;
+                    result = 'hit';
+                    model.score++;
+                    if (ship.hits >= hitsToSink) {
+                        view.displayMessage('You sunk a battleship!');
+                    }
                 }
-                return true;
             }
         }
-        view.displayMiss(guess);
-        view.displayMessage("It's a miss!");
-        return false;
-    },
 
-    isSunk: function(ship) {
-        for (let i = 0; i < this.shipLength; i++) {
-            if (ship.hits[i] !== "hit") {
-                return false;
-            }
+        // sets game history
+        model.history[fieldId] = result;
+
+
+        // display shoot outcome
+        if (result === 'miss') {
+            view.displayMiss(fieldId);
+            view.displayMessage('Miss!');
+        } else {
+            view.displayHit(fieldId);
+            view.displayMessage('You hit a battleship!');
         }
-        return true;
-    }
-};
 
-let view = {
-    displayMessage: function(msg) {
-        let messageArea = document.getElementById("messageArea");
-        messageArea.innerHTML = msg;
-    },
+        // disable further clicks on this item
+        $('#' + fieldId).parent().unbind("click");
 
-    displayHit: function(location) {
-        let cell = document.getElementById(location);
-        cell.setAttribute("class","hit");
 
-    },
-
-    displayMiss: function(location) {
-        let cell = document.getElementById(location);
-        cell.setAttribute("class","miss");
-    }
-};
-
-let controller = {
-    guesses: 0,
-    processGuess: function(location) {
-        if (location) {
-            this.guesses++;
-            let hit = model.fire(location);
-            if (hit && model.shipsSunk === model.numShips) {
-                view.displayMessage("You sunk all of my battleships in " + this.guesses + " tries.");
-                let end = document.getElementById("guessInput").disabled = true;
-            }
+        // checks if game has ended
+        if (model.score === gameWinScore) {
+            view.displayWinn();
         }
     }
-};
 
-function init() {
-    $('#game-init-button').hide();
-    $('#board').show();
-    console.log('game init');
-    let guessClick = document.getElementsByTagName("td");
-    for (let i = 0; i < guessClick.length; i++) {
-        guessClick[i].onclick = answer;
+    return {
+        init
     }
-
-    view.displayMessage("Hello, let's play! There are 3 ships, each 3 cells long");
-}
-
-function answer(eventObj) {
-    let shot = eventObj.target;
-    let location = shot.id;
-    controller.processGuess(location);
-}
-return{
-    init,
-    answer
-}
 })();
 
 
