de40a96d0690894e85e5e2221e001ed45c201566
Theodor <tiapko@abv.bg>
2017-09-10 16:26:15 +0300

    Local and remote history added. Remote history updated after each shoot. Game cannot be started again from the beginning(aim);

--- js/game/battleship.js
+++ js/game/battleship.js
@@ -38,15 +38,15 @@ let playground = (() => {
 
     function init() {
 
-        // hard coded map ID
-
-        // get all maps from database
+        // gets all maps from database
         requester.get('appdata', 'gameBoards', '').then((boardsInfo) => {
+
+            // gets the maps the user has played
             requester.get('appdata', `gamesPlayed?query={"userId":"${sessionStorage.getItem('userId')}"}`, '').then((playedMapsInfo) => {
 
                 // all maps that has been started or completed
                 let mapsStarted = [];
-                let mapsAvailable= false;
+                let mapsAvailable = false;
 
                 for (let map of playedMapsInfo) {
                     mapsStarted.push(map.gameId);
@@ -57,11 +57,24 @@ let playground = (() => {
 
                     // if the map has not been started yet it is loaded
                     if (!mapsStarted.includes(board._id)) {
+                        sessionStorage.setItem('boardId', board._id);
+                        // MAP INITIALIZATION
+                        let historyData = {
+                            gameId: board._id,
+                            userId: sessionStorage.getItem('userId'),
+                            score: 40,
+                            boardProgress: model.history
+                        };
+
+                        requester.post('appdata', 'gamesPlayed', '', historyData).then((req) => {
+                            sessionStorage.setItem('historyId', req._id);
+                        });
+
 
                         mapsAvailable = true;
 
                         // get ships location from the database
-                        requester.get('appdata', 'gameBoards/'+board._id, '').then((mapInfo) => {
+                        requester.get('appdata', 'gameBoards/' + board._id, '').then((mapInfo) => {
                             for (let i = 0; i < mapInfo.board.length; i++) {
                                 let ship = {hits: 0, coordinates: mapInfo.board[i].coordinates};
                                 model.ships.push(ship);
@@ -80,11 +93,12 @@ let playground = (() => {
                         }
                         view.displayMessage("Let's play! There are 3 ships, each 3 cells long");
                         break;
-
                     }
                 }
-                if(!mapsAvailable){
-                    view.displayMessage("You have played all the maps !");
+
+                // if all maps has been played, game cannot start.
+                if (!mapsAvailable) {
+                    view.displayMessage("You have played all available maps!");
                 }
             });
         });
@@ -113,9 +127,8 @@ let playground = (() => {
             }
         }
 
-        // sets game history
+        // sets game local history
         model.history[fieldId] = result;
-
 
         // display shoot outcome
         if (result === 'miss') {
@@ -127,6 +140,19 @@ let playground = (() => {
             view.displayMessage('You hit a battleship!');
         }
 
+        // set game remote history
+        let historyData = {
+            gameId: sessionStorage.getItem('boardId'),
+            userId: sessionStorage.getItem('userId'),
+            score: model.points,
+            boardProgress: model.history
+        };
+
+        requester.update('appdata', 'gamesPlayed/' + sessionStorage.getItem('historyId'), '', historyData).then((data) => {
+            console.log('move saved');
+
+        });
+
         // disable further clicks on this item
         $('#' + fieldId).parent().unbind("click");
 
@@ -134,6 +160,10 @@ let playground = (() => {
         // checks if game has ended
         if (model.score === gameWinScore) {
             view.displayWinn(model.points);
+            model.ships = [];
+            model.score = 0;
+            model.history = new Array(fields).fill('unknown');
+            model.points = startingPoints;
         }
     }
 
