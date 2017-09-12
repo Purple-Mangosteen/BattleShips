58a25e0debcd9d50d1fffba46e50e96611d16119
iozzilina <iova_dineva@yahoo.com>
2017-09-12 22:59:02 +0300

    removed battleshio from template cal

--- js/app.js
+++ js/app.js
@@ -51,11 +51,11 @@ function startApp() {
                     ctx.gameCount = count['count'];
 
                     ctx.loadPartials({
-                        header: '/battleship/templates/common/header.hbs',
-                        footer: '/battleship/templates/common/footer.hbs',
-                        home: '/battleship/templates/home/home.hbs'
+                        header: './templates/common/header.hbs',
+                        footer: '/templates/common/footer.hbs',
+                        home: './templates/home/home.hbs'
                     }).then(function () {
-                        this.partial('/battleship/templates/home/homePage.hbs');
+                        this.partial('./templates/home/homePage.hbs');
                     });
 
                 }).catch(notifier.handleError);
