6ac052f05eba159f745a5af80d88758f6432de51
iozzilina <iova_dineva@yahoo.com>
2017-09-12 17:18:20 +0300

    /

--- js/app.js
+++ js/app.js
@@ -51,11 +51,11 @@ function startApp() {
                     ctx.gameCount = count['count'];
 
                     ctx.loadPartials({
-                        header: './../templates/common/header.hbs',
-                        footer: './../templates/common/footer.hbs',
-                        home: './../templates/home/home.hbs'
+                        header: '/templates/common/header.hbs',
+                        footer: '/templates/common/footer.hbs',
+                        home: '/templates/home/home.hbs'
                     }).then(function () {
-                        this.partial('./../templates/home/homePage.hbs');
+                        this.partial('/templates/home/homePage.hbs');
                     });
 
                 }).catch(notifier.handleError);
